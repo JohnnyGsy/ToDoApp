82f51a022166bc71042d6bc2ea81106debbecece
JohnnyGsy <vanstrelok@gmail.com>
2021-07-28 16:35:48 +0300

    fixing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,79 +1,48 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
-
-  # GET /projects
-  # GET /projects.json
-  def index
-    @user_id = User.find_by(email: current_user.email).id
-    @projects = Project.where(user_id: @user_id)
-    @tasks = Task.where(project_id: @projects) #cut down extra projects, list only current user tasks
-  end
-
-  # GET /projects/1
-  # GET /projects/1.json
-  def show
-  end
-
-  # GET /projects/new
-  def new
-    @project = Project.new
-    #@user_id = User.find_by(email: current_user.email).id
-  end
-
-  # GET /projects/1/edit
-  def edit
-    #@user_id = User.find_by(email: current_user.email).id
-  end
+  before_action :set_project, only: [ :destroy, :editProjectName ] #:show, :edit, :update
 
   # POST /projects
-  # POST /projects.json
   def create
-    @project = Project.new(project_params)
+    @project = Project.new(project_params) 
     @task = Task.new
+
     respond_to do |format|
       if @project.save
-        format.html { render :partial => "pages/show_project", :locals => { :project => @project } }   #redirect_to todolist_url, notice: 'Project was successfully created.'
-                                             # , :locals => { :project => @project }, collection: @task
+        format.html { render :partial => "projects/show_project", :locals => { :project => @project } } 
         #format.json { render action: 'show', status: :created, location: @project }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
-  def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to todolist_url, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
+        format.html { render html: "Error, can not save... reload page and try again" }
         format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { head :no_content } #redirect to To Do List   redirect_to todolist_url
+      format.html { head :no_content } 
       #format.json { head :no_content }
     end
   end
 
   def editProjectName
-    Project.find_by(id: params[:id]).update(name: params[:name])
+    if have_access(@project) then 
+      @project.update(project_params)
+    else 
+      flash[:warning] = "Something went wrong, try again. Can not find project by given id or you have no permission"
+      #render html: 'can not find project by given id or you have no permission'
+    end
   end
 
   private
+    def have_access(project)
+     return true if project  && (project.user_id == current_user.id)  
+     false   
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+         @project = Project.find(params[:id] || params[:project][:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,106 +1,54 @@
-class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
-  # GET /tasks
-  # GET /tasks.json
-  def index
-    @projects = Project.where(user_id: User.find_by(email: current_user.email).id) #current user projects
-
-    @tasks = Task.where(project_id: @projects)
-    
-  end
-
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
-  end
-
-  # GET /tasks/new
-  def new
-    @task = Task.new
-    @project_id = '' #WTF?
-  end
-
-  # GET /tasks/1/edit
-  def edit
-  end
-
-  # POST /tasks
-  # POST /tasks.json
-  def create
-    @task = Task.new(task_params) 
-
-    @projects = Project.where(user_id: User.find_by(email: current_user.email).id) #current user projects
-    @tasks = Task.where(project_id: @projects)
-
-    respond_to do |format|
-      if @task.save
-        format.html { render :partial => "tasks/form_for_filling_project_by_one_task", :locals => { :task => @task } } # redirect_to todolist_url, notice: 'Task was successfully created.'
-        #format.json { render action: 'show', status: :created, location: @task }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
-  def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
-  def destroy
-    @task.destroy
-    respond_to do |format|
-      format.html { head :no_content }
-      #format.json { head :no_content }
-    end
-  end
-
-  def editTaskStatus
-     Task.find_by(id: params[:id]).update(status: params[:value])
-  end 
-
-  def editTaskName
-    Task.find_by(id: params[:id]).update(name: params[:name])
-  end
-
-  def setDeadline
-    Task.find_by(id: params[:id]).update(deadline: params[:deadline])
-  end
-
-  def swapTasks
-    rating_one = Task.find_by(id: params[:id_one]).rating
-    rating_two = Task.find_by(id: params[:id_two]).rating
-    
-    Task.find_by(id: params[:id_one]).update(rating: rating_two)
-    Task.find_by(id: params[:id_two]).update(rating: rating_one)
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def task_params
+  class TasksController < ApplicationController
+    before_action :set_task, only: [:destroy, :editTaskName, :editTaskStatus, :setDeadline]
+  
+   
+    def create
+      @task = Task.new(task_params) 
       
-      #params[:project_id] = Project.find(8)
-      params.require(:task).permit(:rating, :name, :status, :project_id, :value, :deadline)
+      respond_to do |format|
+        if @task.save
+          format.html { render :partial => "tasks/form_for_filling_project_by_one_task", :locals => { :task => @task } } 
+        else
+          format.html { head :no_content }
+        end
+      end
     end
-end
-  #def editTaskStatus
-  #  Tasks.find_by(id: task_params[:id]).update(status: task_params[:value])
-  #end 
+  
+    def destroy
+      @task.destroy
+      respond_to do |format|
+        format.html { head :no_content }
+      end
+    end
+  
+    def editTaskStatus
+      @task.update(task_params)
+    end 
+  
+    def editTaskName
+      @task.update(task_params)
+    end
+  
+    def setDeadline
+      @task.update(task_params)
+    end
+  
+    def swapTasks
+      rating_one = Task.find_by(id: params[:id_one]).rating
+      rating_two = Task.find_by(id: params[:id_two]).rating
+      
+      Task.find_by(id: params[:id_one]).update(rating: rating_two)
+      Task.find_by(id: params[:id_two]).update(rating: rating_one)
+    end
+  
+    private
+     
+      def set_task
+        @task = Task.find(params[:id] || params[:task][:id])
+      end
+  
+   
+      def task_params
+        params.require(:task).permit(:rating, :name, :status, :project_id, :value, :deadline)
+      end
+  end
